3ec14ae2c5fa638473521df47aa4b1eaa0d72e19
Sonanielectronics <100422141+Sonanielectronics@users.noreply.github.com>
2022-11-14 16:29:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -45,7 +45,8 @@ socketIO.on('connection', (socket) => {
 
         let data2 = new Todo({
 
-          username: data.userName
+          username: data.userName,
+          socketid: data.socketID
 
         })
 
