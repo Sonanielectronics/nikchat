82b7bea63a7c135477944426ef324399399ed6c6
Sonanielectronics <100422141+Sonanielectronics@users.noreply.github.com>
2022-11-14 17:09:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ socketIO.on('connection', (socket) => {
       socket.on('disconnect', () => {
         console.log('🔥: A user disconnected');
         users = users.filter(user => user.socketID !== socket.id)
-        await Todo.find({ socketID: "socket.id" }).deleteOne();
+        await Todo.find().deleteOne();
         socketIO.emit("newUserResponse", users)
         socket.disconnect()
       });
